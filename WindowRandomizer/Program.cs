5a8ce412d888fe4f75e538977887f91d1cf00fab
sudaox2001 <107582771+sudaox2001@users.noreply.github.com>
2022-07-24 19:11:42 +0100

    Add files via upload

---
+++ WindowRandomizer/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace WindowRandomizer
+{
+    class Program
+    {
+        [DllImport("user32.dll")]
+        static extern int SetWindowText(IntPtr hWnd, string text);
+
+        static void Main(string[] args)
+        {
+            Process[] proclist = Process.GetProcesses();
+            foreach (Process p in proclist)
+            {
+                string windowTitle = p.MainWindowTitle;
+                char[] chars = windowTitle.ToArray();
+                Random r = new Random(259);
+                for (int i = 0; i < chars.Length; i++)
+                {
+                    int randomIndex = r.Next(0, chars.Length);
+                    char temp = chars[randomIndex];
+                    chars[randomIndex] = chars[i];
+                    chars[i] = temp;
+                }
+                var s = new string(chars);
+                Console.WriteLine("Changed title of " + p.ProcessName);
+                SetWindowText(p.MainWindowHandle, s);
+            }
+        }
+    }
+}
